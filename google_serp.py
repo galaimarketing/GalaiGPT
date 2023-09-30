d787d55b52b7c669ecc2118f0f65102d67177893
X <146244496+galaimarketing@users.noreply.github.com>
2023-09-30 22:47:19 +0100

    Update google_serp.py

--- google_serp.py
+++ google_serp.py
@@ -1,24 +1,28 @@
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium_stealth import stealth
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
+import requests
+import json
+import os
+from webdriver_manager.chrome import ChromeDriverManager  # Import ChromeDriverManager
 
 def search_google_web_automation(query):
-    # Set up Chrome WebDriver options
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
-    options.add_argument("--disable-gpu")  # Disable GPU acceleration for headless mode
-    options.add_argument("--no-sandbox")  # Disable sandboxing for headless mode
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option("useAutomationExtension", False)
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
-    # Create Chrome WebDriver using ChromeDriverManager
+    # Use ChromeDriverManager to automatically download and manage chromedriver
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
     stealth(
         driver,
         languages=["en-US", "en"],
         vendor="Google Inc.",
-        platform="Linux",  # Change platform to Linux
+        platform="Linux",
         webgl_vendor="Intel Inc.",
         renderer="Intel Iris OpenGL Engine",
         fix_hairline=True,
@@ -37,22 +41,18 @@ def search_google_web_automation(query):
 
         driver.get(url)
         soup = BeautifulSoup(driver.page_source, "html.parser")
-        search = soup.find_all("div", class_="tF2Cxc")
+        search = soup.find_all("div", class_="yuRUbf")
         for h in search:
             counter = counter + 1
-            title = h.h3.text
+            title = h.a.h3.text
             link = h.a.get("href")
             rank = counter
             results.append(
                 {
-                    "title": h.h3.text,
+                    "title": h.a.h3.text,
                     "url": link,
                     "domain": urlparse(link).netloc,
                     "rank": rank,
                 }
             )
-
-    # Don't forget to close the driver when you're done with it
-    driver.quit()
-
     return results[:3]
